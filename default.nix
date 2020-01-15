30a9392d60256fa838dbdff1e6616f20fcea724e
danbst <abcz2.uprola@gmail.com>
2020-01-15 17:05:41 +0200

    added default.nix

---
+++ default.nix
@@ -0,0 +1,33 @@
+let
+  pkgs = import <nixpkgs> {
+      config = {};
+      overlays = [
+            (self: super: {
+                python3 = super.python3.override {
+                    packageOverrides = pythonself: pythonsuper: {
+                        opencv4 = pythonsuper.toPythonModule (self.opencv4.override {
+                            pythonPackages = pythonself;
+                            enablePython = true;
+                            enableGtk2 = true;
+                            enableFfmpeg = true;
+                            enableTesseract = true;
+                        });
+                    };
+                };
+            })
+      ];
+  };
+
+  pythonEnv = pkgs.python3.withPackages (p: [
+      p.opencv4
+      p.ipython
+      p.numpy
+  ]);
+
+in {
+    shell = pkgs.mkShell {
+        buildInputs = [
+            pythonEnv
+        ];
+    };
+}
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('img.png')
+img = cv2.imread('./data/track-photo/IMG_3825.JPG')
 
 cv2.imshow('img', img)
 cv2.waitKey(0)
