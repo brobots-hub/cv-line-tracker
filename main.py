649e16416244fbf55bfe13da3931f99aae5ab853
andrew4ever <andrewyazura203@gmail.com>
2020-01-15 20:55:53 +0200

    Fix size of window

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import glob
+
 import cv2
 
-img = cv2.imread('./data/track-photo/IMG_3825.JPG')
+for img_path in glob.glob('./data/track-photo/*.JPG'):
+    img = cv2.imread(img_path)
 
-cv2.imshow('img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.namedWindow(img_path, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(img_path, 800, 600)
+
+    cv2.imshow(img_path, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
