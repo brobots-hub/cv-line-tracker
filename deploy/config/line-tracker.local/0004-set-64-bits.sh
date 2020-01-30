7762bfc0a1946c287c698083572093a3c0e98ee1
andrew4ever <andrewyazura203@gmail.com>
2020-01-30 17:21:50 +0200

    Fix grep output

--- deploy/config/line-tracker.local/0004-set-64-bits.sh
+++ deploy/config/line-tracker.local/0004-set-64-bits.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-if grep "arm_64bit=1" /boot/config.txt
+if grep -q "arm_64bit=1" /boot/config.txt
 then
+    echo "Already using 64-bit kernel"
     exit
 else
     echo "arm_64bit=1" | sudo tee -a /boot/config.txt 
+    echo "Switched to 64-bit kernel"
 fi
-exit
