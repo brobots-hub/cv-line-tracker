da173cb64dbe3b136a1dc925f2ad6f6bad6fac9a
MichaelBozhanov <scrapmecanic3000@gmail.com>
2020-02-04 16:22:06 +0200

    Fixed errors

--- deploy/config/line-tracker.local/0007-change-default-password.sh
+++ deploy/config/line-tracker.local/0007-change-default-password.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config     #changing variable which responsible for ssh password authentication
+
+sudo sed -i 's/#PasswordAuthentication .*/PasswordAuthentication no/g' /etc/ssh/sshd_config     #changing variable which responsible for ssh password authentication
