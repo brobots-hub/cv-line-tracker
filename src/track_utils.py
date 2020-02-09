8207f0223e89759ff8f699eef290c7a7c562da67
MichaelBozhanov <scrapmecanic3000@gmail.com>
2020-02-09 23:12:52 +0200

    added filter

--- src/track_utils.py
+++ src/track_utils.py
@@ -1,4 +1,23 @@
 import cv2
+import numpy as np
+
+def filter_contours(contours, step):
+    filtered = []
+    for contour in contours:
+        
+        _,_,w,h = cv2.boundingRect(contour)
+        cnt_area = int(cv2.contourArea(contour))
+        if cnt_area > 1000*(40/step) and w < 400 and w > 50:
+            filtered.append(contour)
+        else:   
+            pass
+    return filtered
+
+
+
+
+
+
 
 def sub_contours(img_origin, y_from, y_to):
     y_from, y_to = max(1,int(y_from)), int(y_to)
@@ -11,33 +30,81 @@ def sub_contours(img_origin, y_from, y_to):
         c += (0, img_origin.shape[0] - y_to)
     return contours
 
+
+
+
+
+
+
+
+
+
 def center(cnt):
     M = cv2.moments(cnt)
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01']/M['m00'])
     return (int(cx), int(cy))
 
+
+
+
+
+
+
+
+
 def track(img, steps=10, y_limit=0.8, debug=False):
     result = []
     step_y = img.shape[0] * y_limit / steps
     nearest_x = img.shape[1] * 0.5
+
+
+
+
+
+
+
+
+
+
     for i in range(0,steps):
-        cnts = sub_contours(img, i*step_y, (i+1)*step_y)
+        cnts = sub_contours(img, i * step_y, (i + 1) * step_y)
+        cnts = filter_contours(cnts, steps)
+
+
+
+
+
+
         if debug:
-            cv2.drawContours(img, cnts, -1, (0,255,0), 3)
+            cv2.drawContours(img, cnts, -1, (0,255,0), 2)
         tracks = []
         for j, cnt in enumerate(cnts):
             try:
                 x,y = center(cnt)
                 tracks.append((x,y))
                 if debug:
-                    cv2.circle(img, (x,y), 3, (255,0,0), 3)
+                    cv2.circle(img, (x, y), 3, (255, 0, 0), 2)
             except ZeroDivisionError:
                 # bad contour
                 pass
+
+
+
+
+
+
+
+
         if len(tracks) > 0:
             min_x = abs(nearest_x - tracks[0][0])
             min_index = 0
+
+
+
+
+
+
             for ind, track in enumerate(tracks):
                 if abs(nearest_x - track[0]) < min_x:
                     min_x = abs(nearest_x - track[0]) 
@@ -46,7 +113,15 @@ def track(img, steps=10, y_limit=0.8, debug=False):
             result.append((x,y))
             nearest_x = x
             if debug:
-                cv2.circle(img, (x,y), 3, (0,0,255), 3)
-    return result
+                cv2.circle(img, (x, y), 3, (0, 0, 255), 2)
+
+
+
+
+
+
+
+
+    return '----------------------------'
 
 
