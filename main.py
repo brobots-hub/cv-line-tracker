c60d35b8b464cedc788b9c5c1f1b8415b48beebe
MichaelBozhanov <scrapmecanic3000@gmail.com>
2020-02-10 14:01:20 +0200

    added filtering, now script draws only correct contours
    made a little mistake

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import src.track_utils as track_utils
 
 for img_path in glob.glob('./data/track-photo/*.JPG'):
     img = cv2.imread(img_path)
-    print(track_utils.track(img, steps=100, y_limit = 0.5, debug=True))
+    print(track_utils.track(img, steps=5, y_limit = 0.5, debug=True))
 
-    cv2.namedWindow(img_path, cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(img_path, 800, 600)
+    #cv2.namedWindow(img_path, cv2.WINDOW_NORMAL)
+    #cv2.resizeWindow(img_path, 800, 600)
 
-    cv2.imshow(img_path, img)
+    #cv2.imshow(img_path, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- src/track_utils.py
+++ src/track_utils.py
@@ -1,43 +1,42 @@
 import cv2
 import numpy as np
+import glob
+
+cv2.namedWindow("img_path", cv2.WINDOW_KEEPRATIO )
+
+
+def filter_color(img):
+    img = cv2.bilateralFilter(img,9,75,75)
+    lower_bound = np.array([30, 30, 30]) 
+    upper_bound = np.array([150, 150,150]) 
+    
+    mask = cv2.inRange(img, lower_bound, upper_bound) 
+
+    result = cv2.bitwise_and(img, img, mask=mask)
+    return result
+
+
+
 
 def filter_contours(contours, step):
     filtered = []
     for contour in contours:
-        
-        _,_,w,h = cv2.boundingRect(contour)
+        l = cv2.arcLength(contour, True)
         cnt_area = int(cv2.contourArea(contour))
-        if cnt_area > 1000*(40/step) and w < 400 and w > 50:
+        if cnt_area > 200*(10/step) and cnt_area < 10000*(10/step):
             filtered.append(contour)
-        else:   
-            pass
     return filtered
-
-
-
-
-
-
 
 def sub_contours(img_origin, y_from, y_to):
     y_from, y_to = max(1,int(y_from)), int(y_to)
     img = img_origin[-y_to:-y_from,:]
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(img, 127, 255, 0)
+    ret, thresh = cv2.threshold(img, 1, 255, cv2.THRESH_BINARY_INV)
     thresh = cv2.bitwise_not(thresh)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
         c += (0, img_origin.shape[0] - y_to)
     return contours
-
-
-
-
-
-
-
-
-
 
 def center(cnt):
     M = cv2.moments(cnt)
@@ -45,66 +44,26 @@ def center(cnt):
     cy = int(M['m01']/M['m00'])
     return (int(cx), int(cy))
 
-
-
-
-
-
-
-
-
-def track(img, steps=10, y_limit=0.8, debug=False):
+def track(img_origin, steps=10, y_limit=0.8, debug=False):
     result = []
-    step_y = img.shape[0] * y_limit / steps
-    nearest_x = img.shape[1] * 0.5
-
-
-
-
-
-
-
-
-
-
+    step_y = img_origin.shape[0] * y_limit / steps
+    nearest_x = img_origin.shape[1] * 0.5
+    img = filter_color(img)
     for i in range(0,steps):
         cnts = sub_contours(img, i * step_y, (i + 1) * step_y)
         cnts = filter_contours(cnts, steps)
-
-
-
-
-
-
-        if debug:
-            cv2.drawContours(img, cnts, -1, (0,255,0), 2)
         tracks = []
+        good_contours = [] 
         for j, cnt in enumerate(cnts):
             try:
                 x,y = center(cnt)
                 tracks.append((x,y))
-                if debug:
-                    cv2.circle(img, (x, y), 3, (255, 0, 0), 2)
             except ZeroDivisionError:
                 # bad contour
                 pass
-
-
-
-
-
-
-
-
         if len(tracks) > 0:
             min_x = abs(nearest_x - tracks[0][0])
             min_index = 0
-
-
-
-
-
-
             for ind, track in enumerate(tracks):
                 if abs(nearest_x - track[0]) < min_x:
                     min_x = abs(nearest_x - track[0]) 
@@ -113,15 +72,12 @@ def track(img, steps=10, y_limit=0.8, debug=False):
             result.append((x,y))
             nearest_x = x
             if debug:
-                cv2.circle(img, (x, y), 3, (0, 0, 255), 2)
+                cv2.circle(img_origin, (x, y), 3, (0, 0, 255), 2)
+            good_contours.append(cnts[min_index])
+        if len(good_contours) >0 :
+            cv2.drawContours(img_origin, good_contours, -1, (255, 0,0 ))
 
-
-
-
-
-
-
-
-    return '----------------------------'
+    
+    return result
 
 
