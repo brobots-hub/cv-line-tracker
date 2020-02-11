324c7a04a134e8481f3ae13f724970f0d5b55e01
Danylo Hlynskyi <abcz2.uprola@gmail.com>
2020-02-11 14:07:27 +0200

    add basic track detection (#13)
    * add basic track detection
    
    * forgot SSH options on ssh-copy-id
    
    * reformat default.nix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import glob
 
 import cv2
+import src.track_utils as track_utils
 
 for img_path in glob.glob('./data/track-photo/*.JPG'):
     img = cv2.imread(img_path)
+    print(track_utils.track(img, steps=30, y_limit = 0.7, debug=True))
 
     cv2.namedWindow(img_path, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(img_path, 800, 600)
