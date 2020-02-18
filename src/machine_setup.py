73684b8dbe3bdcfca140d887eb54d2efe481138f
danbst <abcz2.uprola@gmail.com>
2020-02-18 19:10:51 +0200

    first remote controlled car

--- src/machine_setup.py
+++ src/machine_setup.py
@@ -1,30 +1,62 @@
 from gpiozero import PWMLED
 from gpiozero import LED
 from time import sleep
+import curses
 
 servo = PWMLED(17)
 servo_min = 0.1
-servo_max = 0.3
+servo_max = 0.28
 
-led1 = LED(5)
-led2 = LED(6)
+LEFT = +1
+RIGHT = -1
 
-def back(delay=0.2):
-  led1.on()
-  led2.off()
-  sleep(delay)
-  led1.off()
+motor1 = PWMLED(5)
+motor2 = PWMLED(6)
 
-
-def forward(delay=0.2):
-  led2.on()
-  led1.off()
-  sleep(delay)
-  led2.off()
+def motor(speed=0.1, delay=0.2, eternal=False):
+    if speed >= 0:
+        motor2.value = speed
+        motor1.value = 0
+    else:
+        motor1.value = abs(speed)
+        motor2.value = 0
+    if not eternal:
+        sleep(delay)
+        motor1.value = 0
+        motor2.value = 0
 
 def rotate(angle=0):
   center = (servo_max + servo_min) / 2
   amp = (servo_max - servo_min) / 2
   servo.value = center + angle*amp
 
+actions = {
+        curses.KEY_UP:    lambda: motor(speed=1, eternal=True),
+        curses.KEY_DOWN:  lambda: motor(speed=-1, eternal=True),
+        curses.KEY_LEFT:  lambda: rotate(LEFT),
+        curses.KEY_RIGHT: lambda: rotate(RIGHT),
+}
 
+def main(window):
+    next_key = None
+    while True:
+        curses.halfdelay(1)
+        if next_key is None:
+            key = window.getch()
+        else:
+            key = next_key
+            next_key = None
+        if key != -1:
+            # KEY PRESSED
+            curses.halfdelay(3)
+            action = actions.get(key)
+            if action is not None:
+                action()
+            next_key = key
+            while next_key == key:
+                next_key = window.getch()
+            # KEY RELEASED
+            motor(speed=0, eternal=True)
+
+def run_machine_v1():
+    curses.wrapper(main)
