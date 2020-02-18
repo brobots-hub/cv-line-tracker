6b48af7725d67349ddc39532d0a91b254e4ffde9
andrew4ever <andrewyazura203@gmail.com>
2020-02-18 20:27:09 +0200

    Add multiprocessing to hardware services

--- src/server/services/move_servo.py
+++ src/server/services/move_servo.py
@@ -1,5 +1,33 @@
 import logging
+import multiprocessing
+import time
 
 
 def move_servo(angle, duration):
+    """Move servo and return it back without blocking main process
+
+    Parameters
+    ----------
+    angle: int
+        Angle servo will move to.
+    duration: int
+        Duration (in ms) servo will stay turned for.
+    """
     logging.info('Set servo to %i degrees for %ims', angle, duration)
+
+    move = multiprocessing.Process(
+        target=_move_servo, args=(angle, duration))
+    move.start()
+
+
+def _move_servo(angle, duration):
+    """Actually move servo to specified angle for some duration.
+
+    Parameters
+    ----------
+    angle: int
+        Angle servo will move to.
+    duration: int
+        Duration (in ms) servo will stay turned for.
+    """
+    time.sleep(duration)
--- src/server/services/spin_motors.py
+++ src/server/services/spin_motors.py
@@ -1,5 +1,33 @@
 import logging
+import multiprocessing
+import time
 
 
 def spin_motors(power, duration):
+    """Spin motor without blocking main process
+
+    Parameters
+    ----------
+    power: int
+        Speed of motor.
+    duration: int
+        Duration (in ms) servo will stay turned for.
+    """
     logging.info('Spin motors for %ims at %i', duration, power)
+
+    move = multiprocessing.Process(
+        target=_spin_motors, args=(power, duration))
+    move.start()
+
+
+def _spin_motors(power, duration):
+    """Actually spin motor for some duration
+
+    Parameters
+    ----------
+    power: int
+        Speed of motor.
+    duration: int
+        Duration (in ms) servo will stay turned for.
+    """
+    time.sleep(duration)
