568e57fb60a4f2d627838c378fa4ecbc08dea867
danbst <abcz2.uprola@gmail.com>
2020-03-05 16:00:38 +0200

    fix uv4l service startup

--- deploy/config/common.local/etc/run-uv4l.sh
+++ deploy/config/common.local/etc/run-uv4l.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
+export PATH=/nix/var/nix/profiles/default/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+rm -rf /dev/video*
 uv4l --driver raspicam --encoding h264 \
     --framerate 10 --vflip --hflip --width 320 --height 240 \
     --frame-buffer 2 --enable-server on --device-name video0 \
-    --profile high --server-option '--port=8088'
+    --profile high --server-option '--port=8088' -f -k -v 6 
 
