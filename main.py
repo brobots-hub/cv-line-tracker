439cc6308a98663f493a79609e2ae31b9c63bf27
MichaelBozhanov <scrapmecanic3000@gmail.com>
2020-03-06 13:26:23 +0200

    something

--- main.py
+++ main.py
@@ -2,16 +2,30 @@ import glob
 import cv2
 import src.track_utils as track_utils
 import sys
-
+import requests
+import time
 cap = cv2.VideoCapture('http://'+ sys.argv[1] +':8088/stream/video.mjpeg')
 cv2.namedWindow("video", cv2.WINDOW_NORMAL)
+cv2.namedWindow("video2", cv2.WINDOW_NORMAL)
+alpha_slider_max = 100
+title_window = 'Linear Blend'
+skip_counter = 0
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1);
 while (cap.isOpened()):
+    if skip_counter%2400 == 0:
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
-        print(track_utils.track(frame, steps=10, y_limit = 1, debug=True))
+        a = track_utils.track(frame, steps=20, y_limit=1, debug=True)
+        if len(a) >0:
+            r = requests.post(url="http://" + sys.argv[1] + ":8080/api/v1/servo", data={"angle": a[1]})
+            #r = requests.post(url="http://" + sys.argv[1] + ":8080/api/v1/motor", data={"power": 0.2})
+            #time.sleep(0.1)
+            #r = requests.post(url="http://" + sys.argv[1] + ":8080/api/v1/motor", data={"power": 0.0})
+            #print(a[0])
         cv2.imshow("video",frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    skip_counter+=1
 cap.release()
 cv2.destroyAllWindows()
 
--- src/track_utils.py
+++ src/track_utils.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import math
+import time
+
 
 
 def filter_color(img):
@@ -13,29 +15,27 @@ def filter_color(img):
     result = cv2.bitwise_and(img, img, mask=mask)
     return result
 
-def find_angle(a, b, c):
-    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
-    return round(ang + 360) if ang < 0 else round(ang)
-
-
-
-
 def filter_contours(img,contours, step):
     filtered = []
     for contour in contours:
         _,_,w,_ = cv2.boundingRect(contour)
         cnt_area = int(cv2.contourArea(contour))
-        if cnt_area > ((img.shape[0] * img.shape[1]) * 0.10 / step) and (cnt_area < ((img.shape[0] * img.shape[1]) * 0.1) / step) and w < (img.shape[1]*0.7):
+        if cnt_area > 300:
             filtered.append(contour)
     return filtered
 
 
-def sub_contours(img_origin, y_from, y_to):
-    y_from, y_to = max(1,int(y_from)), int(y_to)
+def sub_contours(img_origin, y_from, y_to, buff):
+    y_from, y_to = max(1, int(y_from)), int(y_to)
     img = img_origin[-y_to:-y_from,:]
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(img, 127, 255, 0)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = cv2.bilateralFilter(img,9,75,75)
+    ret, thresh = cv2.threshold(img, 60, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    th = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
     thresh = cv2.bitwise_not(thresh)
+    cv2.imshow("video2", thresh)
+
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
         c += (0, img_origin.shape[0] - y_to)
@@ -47,15 +47,14 @@ def center(cnt):
     cy = int(M['m01']/M['m00'])
     return (int(cx), int(cy))
 
-def track(img_origin, steps=10, y_limit=0.8, debug=False):
+def track(img_origin, steps=10, y_limit=0.8, debug=False, buff=2):
     result = []
     step_y = img_origin.shape[0] * y_limit / steps
     nearest_x = img_origin.shape[1] * 0.5
     img = filter_color(img_origin)
-
-    for i in range(0,steps):
-        cnts = sub_contours(img, i * step_y, (i+1)* step_y)
-        #cnts = filter_contours(img,cnts, steps)
+    tt = time.time()
+    for i in range(0, steps):
+        cnts = sub_contours(img_origin, i * step_y, (i + 1) * step_y, buff)
         tracks = []
         good_contours = [] 
 
@@ -80,13 +79,16 @@ def track(img_origin, steps=10, y_limit=0.8, debug=False):
             nearest_x = x
             if debug:
                 cv2.circle(img_origin, (x, y), 3, (0, 0, 255), 2)
-    angles = []
+    distances = []
+    print(time.time() - tt)
+
     for i in range(len(result)):
         try:
-            if abs(result[i][0] - result[i-2][0]) < img_origin.shape[1]*0.2 :
+            if abs(result[i][0] - result[i+1][0]) < img_origin.shape[1]*0.3 :
                 cv2.line(img_origin, result[i], result[i + 1], (255, 255, 0), 5)
                 try:
-                    angles.append(find_angle(result[i-2], result[i-1], result[i]))
+                    if (result[i][0] * 0.0008125) > 0.1 and (result[i][0] * 0.0008125) < 0.2:
+                        distances.append(round(result[i][0] * 0.0008125, 2))
                 except:
                     pass
 
@@ -95,4 +97,4 @@ def track(img_origin, steps=10, y_limit=0.8, debug=False):
                 break
         except:
             pass
-    return angles
+    return distances
